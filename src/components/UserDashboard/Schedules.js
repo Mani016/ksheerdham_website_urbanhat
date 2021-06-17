c017c6918425fa2a5bdad3004fee250a0c4242d9
akash <akash.upadhyay@startxlabs.com>
2021-06-17 20:29:56 +0530

    ui fixes

--- src/components/UserDashboard/Schedules.js
+++ src/components/UserDashboard/Schedules.js
@@ -7,7 +7,6 @@ import Loader from "../../pages/loader";
 import Alert from "../../utils/Alert";
 import NoData from "../NoData";
 const Schedules = () => {
-
   const [modalShow, setModalShow] = React.useState(false);
   const [value, onChange] = React.useState(new Date());
   const [data, setData] = React.useState([]);
@@ -15,29 +14,30 @@ const Schedules = () => {
   function GetSubscriptions(date) {
     setPending(true);
     agent.Customers.subscriptionsCalendar(date).then((res) => {
-      setData(res.data)
+      setData(res.data);
       setModalShow(true);
       setPending(false);
     });
   }
   function UpdateSubscription(id, productId, quantity, orderDate) {
-    setPending(true)
-    agent.Customers.updateSubscription(id, productId, { quantity: quantity }).then((res) => {
-      Alert.showToastAlert('success', res.message);
+    setPending(true);
+    agent.Customers.updateSubscription(id, productId, {
+      quantity: quantity,
+    }).then((res) => {
+      Alert.showToastAlert("success", res.message);
       GetSubscriptions(orderDate);
-    })
+    });
   }
 
   return (
     <Fragment>
-
       <div className="d-flex justify-content-center my-5">
         <Calendar
           className="w-100"
           onChange={onChange}
           value={value}
           onClickDay={(value, event) => {
-            GetSubscriptions(new Date(value).setHours(0, 0, 0, 0))
+            GetSubscriptions(new Date(value).setHours(0, 0, 0, 0));
           }}
         />
         {modalShow && (
@@ -47,31 +47,68 @@ const Schedules = () => {
             className="schedule_modal"
             style={{ maxWidth: "550px" }}
           >
-            { data.length === 0 ? <NoData/>:
-            data.map((item, index) =>
-              <div key={index}>
-                {pending ? <Loader height={'250px'} /> :
-                  <div className="datail_card" >
-                    <div className="left">
-                      <div></div>
-                      <img src={item.image ? item.image : DummyProduct} alt="" />
-                      <h3>
-                        {item.product_name} <span>{item.unit_size} {item.unit} </span>
-                      </h3>
-                    </div>
-                    <div className="right">
-                      <div className="quantity">
-                        <span onClick={() => { UpdateSubscription(item.subscription, item.product, item.quantity + 1, item.orderDate) }}>+</span>
-                        <input type="number" value={item.quantity} readOnly />
-                        <span onClick={() => { UpdateSubscription(item.subscription, item.product, item.quantity - 1, item.orderDate) }}>-</span>
+            {data.length === 0 ? (
+              <NoData />
+            ) : (
+              data.map((item, index) => (
+                <div key={index}>
+                  {pending ? (
+                    <Loader height={"250px"} />
+                  ) : (
+                    <div className="datail_card">
+                      <div className="left">
+                        <div></div>
+                        <img
+                          src={item.image ? item.image : DummyProduct}
+                          alt=""
+                        />
+                        <div style={{ width: "100%" }}>
+                          <h3>
+                            {item.product_name}{" "}
+                            <span>
+                              {item.unit_size} {item.unit}{" "}
+                            </span>
+                          </h3>
+                          <div className="right">
+                            <div className="quantity">
+                              <span
+                                onClick={() => {
+                                  UpdateSubscription(
+                                    item.subscription,
+                                    item.product,
+                                    item.quantity + 1,
+                                    item.orderDate
+                                  );
+                                }}
+                              >
+                                +
+                              </span>
+                              <input
+                                type="number"
+                                value={item.quantity}
+                                readOnly
+                              />
+                              <span
+                                onClick={() => {
+                                  UpdateSubscription(
+                                    item.subscription,
+                                    item.product,
+                                    item.quantity - 1,
+                                    item.orderDate
+                                  );
+                                }}
+                              >
+                                -
+                              </span>
+                            </div>
+                            <div className="price">{item.amount}</div>
+                          </div>
+                        </div>
                       </div>
-                      <div className="price">{item.amount}</div>
                     </div>
-
-                  </div>
-                }
-              </div>
-
+                  )}
+                </div>
+              ))
             )}
           </Modal>
         )}
